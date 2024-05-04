7558b5d40bf7dbfc2b56fc3720440e8995a9b134
Georges <georgesghassankhalil@gmail.com>
2024-05-04 19:20:18 +0300

    added delete functionality for projects

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,17 +5,29 @@ import { CardModule } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
 import { cilPlus, cilPencil, cilTrash } from '@coreui/icons';
 import { Router } from '@angular/router';
+import {
+  ButtonCloseDirective,
+  ButtonDirective,
+  ModalBodyComponent,
+  ModalComponent,
+  ModalFooterComponent,
+  ModalHeaderComponent,
+  ModalTitleDirective,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, CardModule, IconModule],
+  imports: [CommonModule, HttpClientModule, CardModule, IconModule, ButtonDirective, ModalComponent, ModalHeaderComponent, ModalTitleDirective, ThemeDirective, ButtonCloseDirective, ModalBodyComponent, ModalFooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit{
   icons = { cilPlus, cilPencil, cilTrash };
   projects: any[] = [];
+  deleteModalVisible = false;
+  projectIdToDelete: number | null = null;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -34,5 +46,32 @@ export class DashboardComponent implements OnInit{
 
   editProject(id: number) {
     this.router.navigate(['/edit-project', id]);
+  }
+
+  openDeleteModal(projectId: number) {
+    this.projectIdToDelete = projectId;
+    this.deleteModalVisible = true;
+  }
+
+  closeDeleteModal() {
+    this.deleteModalVisible = false;
+  }
+
+  handleDeleteModalChange(event: any) {
+    this.deleteModalVisible = event;
+  }
+
+  deleteProject() {
+    if (this.projectIdToDelete !== null) {
+      this.http.delete(`http://localhost:8000/api/projects/${this.projectIdToDelete}`).subscribe(
+        response => {
+          this.closeDeleteModal();
+          this.ngOnInit();
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
