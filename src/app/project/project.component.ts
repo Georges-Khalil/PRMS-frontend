5825ab22fdbab91dd7c28a2c0db924101cbb15b5
Raymond Saliba <raysaliba13@hotmail.com>
2024-05-06 17:26:17 +0300

    clicking on a project takes us to the report page

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,12 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { CardModule } from '@coreui/angular';
+import { IconModule } from '@coreui/icons-angular';
+import { cilPlus, cilPencil, cilTrash } from '@coreui/icons';
+import { Router } from '@angular/router';
+import {
+  ButtonCloseDirective,
+  ButtonDirective,
+  ModalBodyComponent,
+  ModalComponent,
+  ModalFooterComponent,
+  ModalHeaderComponent,
+  ModalTitleDirective,
+  ThemeDirective
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule, CardModule, IconModule, ButtonDirective, ModalComponent, ModalHeaderComponent, ModalTitleDirective, ThemeDirective, ButtonCloseDirective, ModalBodyComponent, ModalFooterComponent],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit{
+  icons = { cilPlus, cilPencil, cilTrash };
+  projects: any[] = [];
+  deleteModalVisible = false;
+  projectIdToDelete: number | null = null;
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {
+    const userId = localStorage.getItem('userId') || '-1';
+    this.http.get<any[]>(`http://localhost:8000/api/user/projects`, { params: { user_id: userId } }).subscribe(
+      (projects: any[]) => {
+        this.projects = projects;
+      },
+    );
+  }
+  
+  onPlusButtonClick() {
+    this.router.navigate(['/create-project']);
+  }
+
+  editProject(id: number) {
+    this.router.navigate(['/edit-project', id]);
+  }
+
+  openDeleteModal(projectId: number) {
+    this.projectIdToDelete = projectId;
+    this.deleteModalVisible = true;
+  }
+
+  closeDeleteModal() {
+    this.deleteModalVisible = false;
+  }
+
+  handleDeleteModalChange(event: any) {
+    this.deleteModalVisible = event;
+  }
+
+  deleteProject() {
+    if (this.projectIdToDelete !== null) {
+      this.http.delete(`http://localhost:8000/api/projects/${this.projectIdToDelete}`).subscribe(
+        response => {
+          this.closeDeleteModal();
+          this.ngOnInit();
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  goToProject(projectId: number) {
+    this.router.navigate(['/reports', projectId]);
+  }
 }
