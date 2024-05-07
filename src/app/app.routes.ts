85f07d0d4d4d058430a56d1ebc87f5cd3a717086
Georges <georgesghassankhalil@gmail.com>
2024-05-07 16:44:29 +0300

    added 2 new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
 import { EditProjectComponent } from './edit-project/edit-project.component';
 import { ProjectComponent } from './project/project.component';
+import { CreateReportComponent } from './create-report/create-report.component';
+import { EditReportComponent } from './edit-report/edit-report.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -14,4 +16,6 @@ export const routes: Routes = [
     { path: 'create-project', component: CreateProjectComponent },
     { path: 'edit-project/:id', component: EditProjectComponent },
     { path: 'project/:id', component: ProjectComponent },
+    { path: 'create-report', component: CreateReportComponent },
+    { path: 'edit-report/:id', component: EditReportComponent },
 ];
---
+++ src/app/create-report/create-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-report.component.html',
+  styleUrl: './create-report.component.css'
+})
+export class CreateReportComponent {
+
+}
